f19cfd48e1cb67ada91baf8539f3b6fc254ff9bf
cqlyj <luoyingjie0721@gmail.com>
2024-11-26 18:25:37 +0800

    add solmateTopHalf and mathMastersTopHalf functions to CompactCodeBase contract. So we can modularize the function  to avoid path explosion issue

--- test/CompactCodeBase.sol
+++ test/CompactCodeBase.sol
@@ -24,4 +24,48 @@ contract CompactCodeBase {
     function mathMastersSqrt(uint256 y) external pure returns (uint256 z) {
         z = MathMasters.sqrt(y);
     }
+
+    function solmateTopHalf(uint256 x) external pure returns (uint256 z) {
+        assembly {
+            let y := x
+
+            z := 181
+            if iszero(lt(y, 0x10000000000000000000000000000000000)) {
+                y := shr(128, y)
+                z := shl(64, z)
+            }
+            if iszero(lt(y, 0x1000000000000000000)) {
+                y := shr(64, y)
+                z := shl(32, z)
+            }
+            if iszero(lt(y, 0x10000000000)) {
+                y := shr(32, y)
+                z := shl(16, z)
+            }
+            if iszero(lt(y, 0x1000000)) {
+                y := shr(16, y)
+                z := shl(8, z)
+            }
+            z := shr(18, mul(z, add(y, 65536)))
+        }
+    }
+
+    function mathMastersTopHalf(uint256 x) external pure returns (uint256 z) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            z := 181
+
+            // This segment is to get a reasonable initial estimate for the Babylonian method. With a bad
+            // start, the correct # of bits increases ~linearly each iteration instead of ~quadratically.
+            let r := shl(7, lt(87112285931760246646623899502532662132735, x))
+            r := or(r, shl(6, lt(4722366482869645213695, shr(r, x))))
+            r := or(r, shl(5, lt(1099511627775, shr(r, x))))
+            // Correct: 16777215 0xffffff
+            r := or(r, shl(4, lt(16777002, shr(r, x))))
+            z := shl(shr(1, r), z)
+
+            // There is no overflow risk here since `y < 2**136` after the first branch above.
+            z := shr(18, mul(z, add(shr(r, x), 65536))) // A `mul()` is saved from starting `z` at 181.
+        }
+    }
 }
