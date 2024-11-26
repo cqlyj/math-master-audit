5572687d44af30b5738801184db1189b5bd87558
cqlyj <luoyingjie0721@gmail.com>
2024-11-26 18:15:34 +0800

    add uniSqrt and mathMastersSqrt functions to CompactCodeBase contract

--- test/CompactCodeBase.sol
+++ test/CompactCodeBase.sol
@@ -7,4 +7,21 @@ contract CompactCodeBase {
     function mulWadUp(uint256 x, uint256 y) external pure returns (uint256 z) {
         z = MathMasters.mulWad(x, y);
     }
+
+    function uniSqrt(uint256 y) external pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+
+    function mathMastersSqrt(uint256 y) external pure returns (uint256 z) {
+        z = MathMasters.sqrt(y);
+    }
 }
